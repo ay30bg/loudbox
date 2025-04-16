9ee39509c223b03d8dd3f3177941a360c3be62a4
ay30bg <117005347+ay30bg@users.noreply.github.com>
2025-04-16 17:14:11 +0100

    Update signup.jsx

--- src/signup.jsx
+++ src/signup.jsx
@@ -47,7 +47,7 @@ function SignUp({ navigateToSignIn }) {
 
         try {
             // Replace with your actual API call
-            const response = await fetch('https://loudbox-backend.vercel.app/api/signup', {
+            const response = await fetch('${REACT_APP_API_URL}/api/signup', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ username, email, password }),
