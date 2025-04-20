113647c93076d15a6bfad98155c53eb24ca91fbb
ay30bg <117005347+ay30bg@users.noreply.github.com>
2025-04-20 19:32:04 +0100

    Update ticketDetailsPage.jsx

--- src/ticketDetailsPage.jsx
+++ src/ticketDetailsPage.jsx
@@ -1,7 +1,7 @@
 // src/TicketDetailsPage.jsx
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import QRCode from 'qrcode.react'; // Correct import
+import QRCode from 'qrcode.react'; 
 import './ticketDetailsPage.css';
 
 function TicketDetailsPage() {
