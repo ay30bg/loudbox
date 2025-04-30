164f3e593083cf4da4a4d018d7d79da6ad48e0dc
retro.dev <117005347+ay30bg@users.noreply.github.com>
2025-04-30 10:41:22 +0100

    Update ProtectedRoute.jsx

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -1,7 +1,7 @@
 // src/components/ProtectedRoute.js
 import React, { useContext } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
-import { AuthContext } from '../context/AuthContext';
+import { AuthContext } from './AuthContext';
 
 function ProtectedRoute({ children }) {
   const { user } = useContext(AuthContext);
